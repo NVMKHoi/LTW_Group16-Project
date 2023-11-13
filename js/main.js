ea5fafa2993ac5871a94641135a15fb34ea54c3b
Nguyen Van Minh Khoi <21130405@st.hcmuaf.edu.vn>
2023-11-13 14:03:18 +0700

    update chuyen de, them login, register, js blah bluh

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 function showMore() { //show an extra row in "chuyen-de-iphone.html"
     document.getElementById('row2').style.display='flex';
+    document.getElementById('more-btn').style.display='none';
+    document.getElementById('less-btn').style.display='block';
+}
+function showLess() { //hide row
+    document.getElementById('row2').style.display='none';
+    document.getElementById('more-btn').style.display='block';
+    document.getElementById('less-btn').style.display='none';
 }
