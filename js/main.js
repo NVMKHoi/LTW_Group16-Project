5ccf4ddf034f1343bd09070cc976b6c843866bf9
HoLam2904 <145566669+HoLam2904@users.noreply.github.com>
2023-11-14 18:04:33 +0700

    update details, edit datlich and add js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function more(){
+    document.getElementById("xm").style.display = 'flex';
+    document.getElementById("more").style.display = 'none';
+    document.getElementById("hide").style.display = 'block';
+}
+
+function hide(){
+    document.getElementById("xm").style.display = 'none';
+    document.getElementById("more").style.display = 'block';
+    document.getElementById("hide").style.display = 'none';
+}
