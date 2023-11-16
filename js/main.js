d44272879acc9e56f08ab2cbaed7e4f9ee096d20
PhamTienLoc <21130429@st.hcmuaf.edu.vn>
2023-11-17 06:21:44 +0700

    add all user-*.html

--- js/main.js
+++ js/main.js
@@ -35,40 +35,45 @@ function showSlides(n) {
 
 // index - tabs
 
-function openTab(Event, tabName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabs-content");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tabs-link");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    Event.currentTarget.className += "active";
-}
+const tabs = document.querySelector(".tabs-head");
+const tabButton = document.querySelectorAll(".tablinks");
+const contents = document.querySelectorAll(".tabcontent");
 
-document.getElementById("defaultOpen").click();
-document.getElementById("defaultOpen").className += " active";
+tabs.onclick = e => {
+    const id = e.target.dataset.id;
+    if (id) {
+        tabButton.forEach(btn => {
+            btn.classList.remove("active");
+        });
+        e.target.classList.add("active");
+
+        contents.forEach(content => {
+            content.classList.remove("active");
+        });
+        const element = document.getElementById(id);
+        element.classList.add("active");
+    }
+}
 
 function showMore() { //show an extra row in "chuyen-de-iphone.html"
-    document.getElementById('row2').style.display='flex';
-    document.getElementById('more-btn').style.display='none';
-    document.getElementById('less-btn').style.display='block';
+    document.getElementById('row2').style.display = 'flex';
+    document.getElementById('more-btn').style.display = 'none';
+    document.getElementById('less-btn').style.display = 'block';
 }
+
 function showLess() { //hide row
-    document.getElementById('row2').style.display='none';
-    document.getElementById('more-btn').style.display='block';
-    document.getElementById('less-btn').style.display='none';
+    document.getElementById('row2').style.display = 'none';
+    document.getElementById('more-btn').style.display = 'block';
+    document.getElementById('less-btn').style.display = 'none';
 }
-function more(){
+
+function more() {
     document.getElementById("xm").style.display = 'flex';
     document.getElementById("more").style.display = 'none';
     document.getElementById("hide").style.display = 'block';
 }
 
-function hide(){
+function hide() {
     document.getElementById("xm").style.display = 'none';
     document.getElementById("more").style.display = 'block';
     document.getElementById("hide").style.display = 'none';
