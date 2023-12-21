e4c8ab3c03c5792ada2544b252e25fcdf81c5f0f
PhamTienLoc <21130429@st.hcmuaf.edu.vn>
2023-12-22 00:40:36 +0700

    Continue working on product.html

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-
-
 // index - tabs
 
 const tabs = document.querySelector(".tabs-head");
@@ -19,6 +17,26 @@ tabs.onclick = e => {
         });
         const element = document.getElementById(id);
         element.classList.add("active");
+    }
+}
+
+// Product Up/Down buttons
+
+function increaseCount(a, b) {
+    var input = b.previousElementSibling;
+    var value = parseInt(input.value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    input.value = value;
+}
+
+function decreaseCount(a, b) {
+    var input = b.nextElementSibling;
+    var value = parseInt(input.value, 10);
+    if (value > 1) {
+        value = isNaN(value) ? 0 : value;
+        value--;
+        input.value = value;
     }
 }
 
